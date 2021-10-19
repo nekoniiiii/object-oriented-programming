f13719ba76467a098a5b8277da6147671fd51a6d
N <72701806+nekoniiiii@users.noreply.github.com>
2021-10-19 20:50:04 +0700

    Create Time2.java

---
+++ hw-4/timeClass/Time2.java
@@ -0,0 +1,132 @@
+package com.timeClass;
+
+public class Time2 {
+	private int hour; 	// 0 - 23
+	private int minute; // 0 - 59
+	private int second; // 0 - 59
+
+	public Time2(){
+		this(0, 0, 0);
+	}
+
+	public Time2(int hour){
+		this(hour, 0, 0);
+	}
+
+	public Time2(int hour, int minute){
+		this(hour, minute, 0);
+	}
+
+	public Time2(int hour, int minute, int second){
+		if (hour < 0 || hour > 23)
+			throw new IllegalArgumentException("hour must be 0-23");
+
+		if (minute < 0 || minute > 59)
+			throw new IllegalArgumentException("minute must be 0-59");
+
+		if (second < 0 || second > 59)
+			throw new IllegalArgumentException("second must be 0-59");
+
+		this.hour = hour;
+		this.minute = minute;
+		this.second = second;
+	}
+
+	public Time2(Time2 time){
+		this(time.getHour(), time.getMinute(), time.getSecond());
+	}
+
+	// Set Methods
+	// set a new time value using universal time; validate the data
+	public void setTime(int hour, int minute, int second) {
+		if(hour < 0 || hour > 23)
+			throw new IllegalArgumentException("hour must be 0-23");
+
+		if(minute < 0 || minute > 59)
+			throw new IllegalArgumentException("minute must be 0-59");
+
+		if(second < 0 || second > 59)
+			throw new IllegalArgumentException("second must be 0-59");
+
+		this.hour = hour;
+		this.minute = minute;
+		this.second = second;
+	}
+
+	// validate and set hour
+	public void setHour(int hour){
+		if(hour < 0 || hour > 23)
+			throw new IllegalArgumentException("hour must be 0-23");
+
+		this.hour = hour;
+	}
+
+	public void setMinute(int minute){
+		if(minute < 0 && minute > 59)
+			throw new IllegalArgumentException("minute must be 0-59");
+
+		this.minute = minute;
+	}
+
+	public void setSecond(int second){
+		if(second < 0 && second > 59)
+			throw new IllegalArgumentException("second must be 0-59");
+
+		this.second = second;
+}
+
+	// Get Methods
+
+	public int getHour(){
+		return hour;
+	}
+
+	public int getMinute(){
+		return minute;
+	}
+
+	public int getSecond(){
+		return second;
+	}
+	
+	// convert to String in universal-time format (HH:MM:SS)
+	public String toUniversalString(){
+		return String.format("%02d:%02d:%02d", getHour(), getMinute(), getSecond());
+	}
+
+	// convert to String in standard-time format (H:MM:SS AM or PM)
+	public String toString(){
+		return String.format("%d:%02d:%02d %s", 
+				((getHour() == 0 || getHour() == 12) ? 12 : getHour() % 12),
+				getMinute(), getSecond(), (getHour() < 12 ? "AM" : "PM"));
+	}
+	
+	// increment time by 1 second
+	public void tick() {
+		if (getSecond() == 59) {
+            setSecond(0);
+            incrementMinute();
+		}
+        else 
+        	setSecond(getSecond() + 1);
+	}
+	
+	// increment time by 1 minute
+	public void incrementMinute() {
+		if (getMinute() == 59) {
+            setMinute(0);
+            incrementHour();
+		}
+        else 
+        	setMinute(getMinute() + 1);
+	}
+	
+	// increment time by 1 hour
+	public void incrementHour() {
+		if (getHour() == 23) {
+            setHour(0);
+		}
+        else 
+        	setHour(getHour() + 1);
+	}
+}
