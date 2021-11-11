9edaa3fb7391d3a45e044b0c1462a8ecb60d8b3a
N <72701806+nekoniiiii@users.noreply.github.com>
2021-11-11 22:38:17 +0700

    Create Employee.java

---
+++ hw-5/employeeHierarchy3/Employee.java
@@ -0,0 +1,29 @@
+package com.employeeHierarchy;
+
+public class Employee {
+  private String firstName;
+	private String lastName;
+	private String socialSecurityNumber;
+	
+	public Employee(String firstName, String lastName, String socialSecurityNumber) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.socialSecurityNumber = socialSecurityNumber;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public String getSocialSecurityNumber() {
+		return this.socialSecurityNumber;
+	}
+	
+	public String toString() {
+		return String.format("Name: " + getFirstName() + " " + getLastName() + "\nSocial security number: " + getSocialSecurityNumber());
+	}
+}
